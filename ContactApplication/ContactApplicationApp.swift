3feb9b8367594c5130a367b4c0babfe19a563e9b
Charles Pink <ccpink99@gmail.com>
2022-07-25 11:25:29 -0300

    Initial Commit

---
+++ ContactApplication/ContactApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactApplicationApp.swift
+//  ContactApplication
+//
+//  Created by Charles Pink on 2022-07-25.
+//
+
+import SwiftUI
+
+@main
+struct ContactApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
